b11433b073f8ce09d754bff44406f553df2f907b
RDF014 <rdf014@gmail.com>
2017-02-13 13:16:12 -0800

    all tests passed

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
 var Bee = function() {
+	Grub.call(this);
+	this.age = 5;
+	this.color = 'yellow';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
+// Bee.age = 5;
+// Bee.Color = yellow;
+
+Bee.prototype.job = 'keep on growing';
 
